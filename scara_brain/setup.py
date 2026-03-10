3415255609670dcee74a5887a79d84b9ec11d2b6
zeyad-shaban <zeyadshapan2004@gmail.com>
2026-03-11 01:36:27 +0200

    added ugly (but funcitoning) brain node for state machine controlling

--- scara_brain/setup.py
+++ scara_brain/setup.py
@@ -24,7 +24,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'test_move = scara_brain.test_move:main'
+            'brain_node = scara_brain.brain_node:main'
         ],
     },
 )
