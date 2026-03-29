063a4acd381f658212a53b03319a1af58acc5ace
zeyad-shaban <zeyadshapan2004@gmail.com>
2026-03-29 20:19:10 +0200

    Auto Alignment basic algoirhtm

--- litho_ws/src/litho_brain/litho_brain/behaviours/movement_beh.py
+++ litho_ws/src/litho_brain/litho_brain/behaviours/movement_beh.py
@@ -1,4 +1,4 @@
-from litho_brain.constants import X_STAGE_NAME, Y_STAGE_NAME, STAGE_STABLE_TIMEOUT, STAGE_JOINT_NAMES, STAGE_Z_BOUNDS, Z_STAGE_NAME
+from litho_brain.constants import X_STAGE_NAME, Y_STAGE_NAME, STAGE_STABLE_TIMEOUT, STAGE_JOINT_NAMES, STAGE_Z_BOUNDS, Z_STAGE_NAME, N_MARKER_ROWS, N_MARKER_COLS, MARKER_HORIZ_GAP, MARKER_VERT_GAP
 from rclpy.action.client import ActionClient, ClientGoalHandle, GoalStatus
 from control_msgs.action import FollowJointTrajectory
 from litho_brain.utils.movement_utils import build_goal
@@ -21,8 +21,8 @@ class GoToOriginBeh(py_trees.behaviour.Behaviour):
         self._act_client = ActionClient(self.node, FollowJointTrajectory, '/joint_trajectory_controller/follow_joint_trajectory')
         
         self._orig_pos = [
-            -0.021000,
-            -0.021000,
+            -0.020048,
+            -0.020048,
             0.017362,
         ]
         
@@ -55,10 +55,9 @@ class GoToOriginBeh(py_trees.behaviour.Behaviour):
         res_fut.add_done_callback(self._response_cb)
     
     def _response_cb(self, fut):
-        self.node.get_logger().error(f"{self.name} Goal rejected")
         goal_handle: ClientGoalHandle = fut.result()
         if not goal_handle.accepted:
-            self.node.get_logger().warning(f"{self.name} Goal rejected")
+            self.node.get_logger().error(f"{self.name} Goal rejected")
             self._moving = False
             self._failed = True
             return
@@ -77,6 +76,108 @@ class GoToOriginBeh(py_trees.behaviour.Behaviour):
             self._moving = False
             self._failed = True
             
+            
+
+class GoToNextMarker(py_trees.behaviour.Behaviour):
+    def __init__(self, name: str, node: Node, idx, precision_microns=1):
+        super().__init__(name) 
+        self.node = node
+        self._act_client = ActionClient(self.node, FollowJointTrajectory, '/joint_trajectory_controller/follow_joint_trajectory')
+        
+        self._idx = idx
+        self._tolerance_meters = precision_microns*1e-6
+        
+    def initialise(self) -> None:
+        self._joints_sub = self.node.create_subscription(JointState, '/joint_states', self._pos_callback, 10)
+
+        self._moving = False
+        self._failed = False
+        self._done = False
+        
+        self._curr_x: float|None = None
+        self._curr_y: float|None = None
+        self._curr_z: float|None = None
+        
+        if self._idx == N_MARKER_ROWS * N_MARKER_COLS - 1:
+            self.node.get_logger().info(f"{self.name} Not moving as reached final marker, we are done...")
+            self._done = True
+
+    def update(self) -> Status:
+        if self._failed:
+            return Status.FAILURE
+        if self._done:
+            return Status.SUCCESS
+        
+        are_servers_ready = self._act_client.server_is_ready() and self._joints_sub.get_publisher_count() > 0 # todo dont' do this as we cna't tell which server is off
+        if not are_servers_ready or self._curr_x is None or self._curr_y is None or self._curr_z is None:
+            self.node.get_logger().warning(f"{self.name} waiting for autoalignment/correction_vec...")
+            return Status.RUNNING
+
+        if self._moving:
+            return Status.RUNNING
+
+        if not self._moving:
+            self._start_moving()
+            return Status.RUNNING
+        
+        return Status.RUNNING
+
+    def terminate(self, new_status: Status) -> None:
+        if self._joints_sub is not None:
+            self.node.destroy_subscription(self._joints_sub)
+
+    def _pos_callback(self, msg: JointState):
+        idx_x = msg.name.index(X_STAGE_NAME) # type: ignore
+        self._curr_x = msg.position[idx_x]
+        
+        idx_y = msg.name.index(Y_STAGE_NAME) # type: ignore
+        self._curr_y = msg.position[idx_y]
+        
+        idx_z = msg.name.index(Z_STAGE_NAME) # type: ignore
+        self._curr_z = msg.position[idx_z]
+        
+    def _start_moving(self):
+        # decide if to move left or right
+        horiz_offset = 0
+        vert_offset = 0
+        
+        if (self._idx + 1) % N_MARKER_ROWS == 0:
+            horiz_offset = -MARKER_HORIZ_GAP * (N_MARKER_ROWS - 1)
+            vert_offset = MARKER_VERT_GAP
+        else:
+            vert_offset = 0
+            horiz_offset = MARKER_HORIZ_GAP
+        
+        self.node.get_logger().info(f"{self.name} Starting moving - horiz {horiz_offset}, vert {vert_offset}")
+        
+        positions: list[float] = [self._curr_x + vert_offset, self._curr_y + horiz_offset, self._curr_z] # type: ignore
+        
+        self._moving = True
+        goal = build_goal(STAGE_JOINT_NAMES, positions, duration_sec=1, pos_tolerance=self._tolerance_meters)
+        
+        self._act_client.send_goal_async(goal).add_done_callback(self._response_callback)
+        
+    def _response_callback(self, fut):
+        response: ClientGoalHandle = fut.result()
+        
+        if not response.accepted:
+            self.node.get_logger().error(f"{self.name} Goal Rejected...")
+            self._failed = True
+            
+        response.get_result_async().add_done_callback(self._result_callback)
+        
+    def _result_callback(self, fut):
+        status: GoalStatus = fut.result().status
+        result: FollowJointTrajectory.Result = fut.result().result
+        
+        if status == GoalStatus.STATUS_SUCCEEDED:
+            self._moving = False
+            self._done = True
+        else:
+            self.node.get_logger().error(f"{self.name} Goal Failed...")
+            self._failed = True
+            self._moving = False
+        
             
 # todo the control system here fucken sucks, at least ad a feedback loop to update the values as we are going instead of overshooting this bdly
 class AutoAlignmentBeh(py_trees.behaviour.Behaviour):
@@ -106,7 +207,7 @@ class AutoAlignmentBeh(py_trees.behaviour.Behaviour):
         
         are_servers_ready = self._correction_vec_sub.get_publisher_count() > 0 and self._act_client.server_is_ready() and self._joints_sub.get_publisher_count() > 0
         if not are_servers_ready or self._offset_x is None or self._offset_y is None or self._curr_x is None or self._curr_y is None or self._curr_z is None:
-            self.node.get_logger().warning(f"{self.name} waiting for autoalignment/correction_vec...")
+            self.node.get_logger().warning(f"{self.name} waiting for servers to start...") # todo acutlly tell the servers required
             return Status.RUNNING
         if self._moving:
             return Status.RUNNING
@@ -123,6 +224,8 @@ class AutoAlignmentBeh(py_trees.behaviour.Behaviour):
     def terminate(self, new_status: Status) -> None:
         if self._correction_vec_sub is not None:
             self.node.destroy_subscription(self._correction_vec_sub)
+        if self._joints_sub is not None:
+            self.node.destroy_subscription(self._joints_sub)
         
     def _pos_callback(self, msg: JointState):
         idx_x = msg.name.index(X_STAGE_NAME) # type: ignore
--- litho_ws/src/litho_brain/litho_brain/trees/litho_tree.py
+++ litho_ws/src/litho_brain/litho_brain/trees/litho_tree.py
@@ -1,4 +1,4 @@
-from litho_brain.behaviours.movement_beh import GoToOriginBeh, AutoAlignmentBeh
+from litho_brain.behaviours.movement_beh import GoToOriginBeh, AutoAlignmentBeh, GoToNextMarker
 from litho_brain.behaviours.focus_beh import AutoFocusBeh
 import py_trees
 from py_trees.composites import Sequence, Selector, Parallel
@@ -8,10 +8,12 @@ from rclpy.node import Node
 
 def get_root(node: Node):
     root = py_trees.composites.Sequence(name='lol', memory=True)
-    
     root.add_child(GoToOriginBeh('GoToOrigin', node))
-    # for die in dies:
+    
     root.add_child(AutoFocusBeh('Auto focus', node))
-    root.add_child(AutoAlignmentBeh('Auto Alignment', node, min_thresh_micron=10))
-    # root.add_child(DLPExpose)
+    dies = [{'some info about die i guess...'}]*25
+    for i, die in enumerate(dies):
+        root.add_child(AutoAlignmentBeh('Auto Alignment', node, min_thresh_micron=10))
+        # root.add_child(DLPExpose)
+        root.add_child(GoToNextMarker(f'GoToMarker {i}', node, i))
     return root
